64430c3f93cf7757334de8d3b6ac2853ac27434a
Batka <batb73979@gmail.com>
2023-04-29 21:25:50 +0800

    my full button

---
+++ app/src/main/java/eaye/my/app/battsooj/myfullbutton/MainActivity.java
@@ -0,0 +1,14 @@
+package eaye.my.app.battsooj.myfullbutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
